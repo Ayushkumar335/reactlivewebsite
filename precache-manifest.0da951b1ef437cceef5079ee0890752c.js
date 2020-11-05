07a15c303a1396d874893df1f4ae355b115e5d77
Rishav <rishavk1102@gmail.com>
2020-11-05 12:46:31 +0530

    Updates

---
+++ precache-manifest.0da951b1ef437cceef5079ee0890752c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ce43494ecacd42aeeba09e68215cbd",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "7a8e90a43350f30f085b",
+    "url": "/reactlivewebsite/static/css/main.3755771a.chunk.css"
+  },
+  {
+    "revision": "a1f6a2550a5af73b3126",
+    "url": "/reactlivewebsite/static/js/2.dbdc438a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/reactlivewebsite/static/js/2.dbdc438a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8e90a43350f30f085b",
+    "url": "/reactlivewebsite/static/js/main.9fd19665.chunk.js"
+  },
+  {
+    "revision": "8c978ee9132a067e9e8e",
+    "url": "/reactlivewebsite/static/js/runtime-main.adbfa9dc.js"
+  }
+]);
